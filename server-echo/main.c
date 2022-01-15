566814c8dba3ec1d88957bded04f8ae878bb43dd
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-15 22:09:59 +0000

    Add example of a blocking server

--- server-echo/main.c
+++ server-echo/main.c
@@ -10,6 +10,53 @@
 #include <sys/un.h>
 #define UNIX_SOCK_PATH "/tmp/mysocket"
 
+static void echo_read_cb(struct bufferevent *bev, void *ctx) {
+  /* This callback is invoked when there is data to read on bev. */
+  struct evbuffer *input = bufferevent_get_input(bev);
+  struct evbuffer *output = bufferevent_get_output(bev);
+
+  size_t len = evbuffer_get_length(input);
+  char *data;
+  data = malloc(len);
+  evbuffer_copyout(input, data, len);
+
+  printf("we got some data: %s\n", data);
+
+  /* Copy all the data from the input buffer to the output buffer. */
+  evbuffer_add_buffer(output, input);
+  free(data);
+}
+
+static void echo_event_cb(struct bufferevent *bev, short events, void *ctx) {
+  if (events & BEV_EVENT_ERROR) perror("Error from bufferevent");
+  if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR)) {
+    bufferevent_free(bev);
+  }
+}
+
+static void accept_conn_cb(struct evconnlistener *listener, evutil_socket_t fd,
+                           struct sockaddr *address, int socklen, void *ctx) {
+  /* We got a new connection! Set up a bufferevent for it. */
+  struct event_base *base = evconnlistener_get_base(listener);
+  struct bufferevent *bev =
+      bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+
+  bufferevent_setcb(bev, echo_read_cb, NULL, echo_event_cb, NULL);
+
+  bufferevent_enable(bev, EV_READ | EV_WRITE);
+}
+
+static void accept_error_cb(struct evconnlistener *listener, void *ctx) {
+  struct event_base *base = evconnlistener_get_base(listener);
+  int err = EVUTIL_SOCKET_ERROR();
+  fprintf(stderr,
+          "Got an error %d (%s) on the listener. "
+          "Shutting down.\n",
+          err, evutil_socket_error_to_string(err));
+
+  event_base_loopexit(base, NULL);
+}
+
 int main(int argc, char **argv) {
   struct event_base *base;
   struct evconnlistener *listener;
@@ -23,7 +70,7 @@ int main(int argc, char **argv) {
   }
 
   /* Clear the sockaddr before using it, in case there are extra
-   * platform-specific fields that can mess us up. */
+     platform-specific fields that can mess us up. */
   memset(&sin, 0, sizeof(sin));
   sin.sun_family = AF_LOCAL;
   strcpy(sin.sun_path, UNIX_SOCK_PATH);
